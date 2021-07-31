841c5a308d8457cd818ef18076ecf15118494e5e
William Ta <wta@student.42.fr>
2021-07-31 03:40:18 +0200

    feat: implement store

---
+++ internal/genetic/interface.go
@@ -0,0 +1,7 @@
+package genetic
+
+type Store interface {
+	Duplicate() Store
+	Take(name string, amount int) (int, bool)
+	Store(name string, amount int) int
+}
---
+++ internal/genetic/store.go
@@ -0,0 +1,56 @@
+package genetic
+
+import "github.com/MonsieurTa/krpsim/internal/entity"
+
+type store map[string]int
+
+func NewStore(cfg *entity.Config) Store {
+	rv := store{}
+	rv.init(cfg)
+	return rv
+}
+
+func (s store) Duplicate() Store {
+	rv := make(store, len(s))
+
+	for key, value := range s {
+		rv[key] = value
+	}
+	return rv
+}
+
+func (s store) init(cfg *entity.Config) {
+	for _, stock := range cfg.Stocks {
+		s.Store(stock.Key, stock.Value)
+	}
+
+	for _, process := range cfg.Processes {
+		for _, need := range process.Needs {
+			s.Store(need.Key, 0)
+		}
+		for _, result := range process.Results {
+			s.Store(result.Key, 0)
+		}
+	}
+}
+
+func (s store) Take(name string, amount int) (int, bool) {
+	v, ok := s[name]
+	newAmount := v - amount
+	if !ok || newAmount < 0 {
+		return -1, false
+	}
+	s[name] = newAmount
+	return amount, true
+}
+
+func (s store) Store(name string, amount int) int {
+	v, ok := s[name]
+	if !ok {
+		s[name] = amount
+		return amount
+	}
+	rv := v + amount
+	s[name] = rv
+	return rv
+}
