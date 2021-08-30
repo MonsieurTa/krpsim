0649cfe841a83e8ad69f55224bcbd3e161922c57
William TA <wta@e1r9p16.42.fr>
2021-08-30 16:21:00 +0200

    test wip

--- internal/simulator/simulator_test.go
+++ internal/simulator/simulator_test.go
@@ -17,24 +17,28 @@ func TestIkeaSimulation(t *testing.T) {
 	}
 
 	populationSize := 100
-	genesPerIndividual := 40
+	genesPerIndividual := 100
 
 	simulator := NewSimulator(&SimulatorConfig{
 		KrpSimConfig:               cfg,
 		PopulationSize:             populationSize,
 		GenesPerIndividual:         genesPerIndividual,
 		GenerationLimit:            1000,
-		MutationRate:               0.10,
-		TournamentPoolSize:         4,
+		MutationRate:               0.1,
+		TournamentPoolSize:         genesPerIndividual / 4,
 		TournamentSelectionPortion: 0.25,
 		ElitismRatio:               0.1,
+		CrossoverPoints:            2,
 	})
 	simulator.Init()
 	result := simulator.Run()[0]
 
 	fitness := simulator.GetFitnesss(result)
-	for i := 0; i < int(fitness.Points); i++ {
-		fmt.Println(result.Genes()[i].Name)
+	totalDelay := 0
+	for i := 0; i < int(fitness.SuccessfullGenes); i++ {
+		fmt.Println(totalDelay, result.Genes()[i].Name)
+		totalDelay += result.Genes()[i].Delay
 	}
+	fmt.Println(fitness.Store)
 	fmt.Println("score:", fitness.Score, "points:", fitness.Points, "total delay:", fitness.TotalDelay)
 }
