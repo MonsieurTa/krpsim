8c999b946aadcb40b5831866b66c5fe227fa20c8
William TA <wta@e1r9p16.42.fr>
2021-08-30 15:14:38 +0200

    refactor: enhance store

--- internal/store/interface.go
+++ internal/store/interface.go
@@ -1,7 +1,12 @@
 package store
 
+import "github.com/MonsieurTa/krpsim/internal/entity"
+
 type Store interface {
 	Duplicate() Store
-	Take(name string, amount int) (int, bool)
+	Consume(name string, amount int) (int, bool)
+	ConsumeIfAvailable(stocks entity.Stocks) bool
 	Store(name string, amount int) int
+	BatchStore(stocks entity.Stocks)
+	Value() store
 }
--- internal/store/store.go
+++ internal/store/store.go
@@ -20,21 +20,41 @@ func (s store) Duplicate() Store {
 }
 
 func (s store) init(cfg *entity.Config) {
-	for _, stock := range cfg.Stocks {
-		s.Store(stock.Key, stock.Value)
+	for name, value := range cfg.Stocks {
+		s.Store(name, value)
 	}
 
 	for _, process := range cfg.Processes {
-		for _, need := range process.Needs {
-			s.Store(need.Key, 0)
+		for name := range process.Needs {
+			s.Store(name, 0)
 		}
-		for _, result := range process.Results {
-			s.Store(result.Key, 0)
+		for name := range process.Results {
+			s.Store(name, 0)
 		}
 	}
 }
 
-func (s store) Take(name string, amount int) (int, bool) {
+func (s store) ConsumeIfAvailable(stocks entity.Stocks) bool {
+	for name, value := range stocks {
+		if !s.available(name, value) {
+			return false
+		}
+	}
+	for name, value := range stocks {
+		s.Consume(name, value)
+	}
+	return true
+}
+
+func (s store) available(name string, amount int) bool {
+	v, ok := s[name]
+	if !ok || v < amount {
+		return false
+	}
+	return true
+}
+
+func (s store) Consume(name string, amount int) (int, bool) {
 	v, ok := s[name]
 	newAmount := v - amount
 	if !ok || newAmount < 0 {
@@ -53,4 +73,14 @@ func (s store) Store(name string, amount int) int {
 	rv := v + amount
 	s[name] = rv
 	return rv
+}
+
+func (s store) BatchStore(stocks entity.Stocks) {
+	for name, amount := range stocks {
+		s.Store(name, amount)
+	}
+}
+
+func (s store) Value() store {
+	return s
 }
